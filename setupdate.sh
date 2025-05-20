5e1c256880f5a67a2afaa01b7368420a61b2949c
Mentor <mentor@palokaj.co>
2025-05-20 15:12:13 +0200

    🐛 sudo management in update

--- setupdate.sh
+++ setupdate.sh
@@ -15,6 +15,21 @@ mkdir -p "$BINARY_FOLDER"
 curl -fsSL "$FILE_URL" | sudo tee "$BINARY_FILE" > /dev/null
 
 # Make it executable
-chown $USER "$BINARY_FILE"
-chmod 755 "$BINARY_FILE"
-chmod u+x "$BINARY_FILE"
+
+# If user is not owner, change ownership
+if [ "$(stat -c '%U' "$BINARY_FILE")" != "$USER" ]; then
+  echo "Changing ownership of $BINARY_FILE to $USER"
+  sudo chown $USER "$BINARY_FILE"
+fi
+
+# If permissions not 755, change them
+if [ "$(stat -c '%a' "$BINARY_FILE")" != "755" ]; then
+  echo "Changing permissions of $BINARY_FILE to 755"
+  sudo chmod 755 "$BINARY_FILE"
+fi
+
+# If not yet executable, make it executable
+if [ ! -x "$BINARY_FILE" ]; then
+  echo "Making $BINARY_FILE executable"
+  sudo chmod +x "$BINARY_FILE"
+fi
